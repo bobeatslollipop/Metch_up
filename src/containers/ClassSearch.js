0fdf104147cc653ec6beaf483951b620d1e24c20
ZhuoranS <zhuoran.su.ufo@gmail.com>
2020-05-11 21:13:35 -0500

    Update ClassSearch.js

--- src/containers/ClassSearch.js
+++ src/containers/ClassSearch.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { ListGroup, ListGroupItem, Row, Col, Button, Container } from "react-bootstrap";
+import { ListGroup, ListGroupItem, Row, Col, Button, Container, FormControl } from "react-bootstrap";
 import data from "../data/4770/courses.json";
 import { useState } from 'react';
 import "./ClassSearch.css";
@@ -60,7 +60,12 @@ export default function ClassSearch(props) {
   return (
     <Container>
       <ListGroup>
-        <input type="text" placeholder="Enter class to search" style={elementStyle} onChange={e => setInput(e.target.value)} block />
+        <FormControl
+          aria-label="Default"
+          aria-describedby="inputGroup-sizing-default"
+          onChange={e => setInput(e.target.value)}
+          placeholder="Enter class to search"
+        />
         {courses}
         <h4>{search_text}</h4>
       </ListGroup>
