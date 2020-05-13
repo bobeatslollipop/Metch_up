e55e65ce430a5df98793c76289a7204a7b9ae887
ZhuoranS <zhuoran.su.ufo@gmail.com>
2020-05-13 13:55:56 -0500

    Weird Design

--- src/containers/UserCenter.js
+++ src/containers/UserCenter.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Col, Row, Container } from "react-bootstrap";
+import { Col, Row, Tab, Nav, Container, Jumbotron } from "react-bootstrap";
 import "./UserCenter.css";
 import { LinkContainer } from "react-router-bootstrap";
 import { Auth } from "../firebase";
@@ -7,10 +7,41 @@ import { Auth } from "../firebase";
 export default function UserCenter(props) {
   //Any ideas what to put here?
   //Username, Dorm, ClassYear, Major, Introduction
-  
+
   return (
-    <Container className="UserCenter">
-      
+    <Container>
+      <Jumbotron className="UserCenter">
+        <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+          <Row>
+            <Col sm={3}>
+              <Nav variant="pills" className="flex-column">
+                <Nav.Item>
+                  <Nav.Link eventKey="first">User Profile</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="second" >Account Settings</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="third" >About Us</Nav.Link>
+                </Nav.Item>
+              </Nav>
+            </Col>
+            <Col sm={9}>
+              <Tab.Content>
+                <Tab.Pane eventKey="first">
+                  lol
+                </Tab.Pane>
+                <Tab.Pane eventKey="second">
+                  lmao
+                </Tab.Pane>
+                <Tab.Pane eventKey="third">
+                  Northwestern University
+                </Tab.Pane>
+              </Tab.Content>
+            </Col>
+          </Row>
+        </Tab.Container>
+      </Jumbotron>
     </Container>
   );
 }
