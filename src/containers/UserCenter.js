6b7753b491738682b7cccc5357fbd23236e5ae33
ZhuoranS <zhuoran.su.ufo@gmail.com>
2020-05-14 00:05:45 -0500

    Update UserCenter.js

--- src/containers/UserCenter.js
+++ src/containers/UserCenter.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Col, Row, Tab, Nav, Container, Jumbotron } from "react-bootstrap";
+import { Col, Row, Tab, Nav, Container, Jumbotron, Card, ListGroup, ListGroupItem } from "react-bootstrap";
 import "./UserCenter.css";
 import { LinkContainer } from "react-router-bootstrap";
 import { Auth } from "../firebase";
@@ -7,6 +7,72 @@ import { Auth } from "../firebase";
 export default function UserCenter(props) {
   //Any ideas what to put here?
   //Username, Dorm, ClassYear, Major, Introduction
+  function CreatorInfo(props){
+    return(
+      <Row>
+        <Col md={6}>
+          <Card>
+            <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+            <Card.Body>
+              <Card.Title>{props.name}</Card.Title>
+              <Card.Text>
+                Creator of the Metchup.
+              </Card.Text>
+            </Card.Body>
+            <ListGroup className="list-group-flush">
+              <ListGroupItem>WCAS 23'</ListGroupItem>
+              <ListGroupItem>Major:Computer Science</ListGroupItem>
+              <ListGroupItem>Frontend developer</ListGroupItem>
+            </ListGroup>
+            <Card.Body>
+              <Card.Link href="">Github</Card.Link>
+              <Card.Link href="https://www.linkedin.com/in/zrsu/" target="_blank">Linkedin</Card.Link>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col md={6}>
+          <Card>
+            <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+            <Card.Body>
+              <Card.Title>{props.name}</Card.Title>
+              <Card.Text>
+                Creator of the Metchup.
+              </Card.Text>
+            </Card.Body>
+            <ListGroup className="list-group-flush">
+              <ListGroupItem>WCAS 23'</ListGroupItem>
+              <ListGroupItem>Major:Computer Science</ListGroupItem>
+              <ListGroupItem>Frontend developer</ListGroupItem>
+            </ListGroup>
+            <Card.Body>
+              <Card.Link href="#">Github</Card.Link>
+              <Card.Link href="#">Linkedin</Card.Link>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col md={6}>
+          <Card>
+            <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+            <Card.Body>
+              <Card.Title>{props.name}</Card.Title>
+              <Card.Text>
+                Creator of the Metchup.
+              </Card.Text>
+            </Card.Body>
+            <ListGroup className="list-group-flush">
+              <ListGroupItem>WCAS 23'</ListGroupItem>
+              <ListGroupItem>Major:Computer Science</ListGroupItem>
+              <ListGroupItem>Frontend developer</ListGroupItem>
+            </ListGroup>
+            <Card.Body>
+              <Card.Link href="#">Github</Card.Link>
+              <Card.Link href="#">Linkedin</Card.Link>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    )
+  }
 
   return (
     <Container>
@@ -35,7 +101,7 @@ export default function UserCenter(props) {
                   What
                 </Tab.Pane>
                 <Tab.Pane eventKey="third">
-                  Northwestern University
+                  <CreatorInfo name="First Person"></CreatorInfo>
                 </Tab.Pane>
               </Tab.Content>
             </Col>
