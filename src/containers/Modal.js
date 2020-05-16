1bdcb018a098d46ab43a41e962460157849411a8
ZhuoranS <zhuoran.su.ufo@gmail.com>
2020-05-15 20:46:33 -0500

    Update Modal.js

--- src/containers/Modal.js
+++ src/containers/Modal.js
@@ -8,41 +8,46 @@ import { getUserByClass } from "../firebase"
 
 export default function ClassModal(props) {
     const [show, setShow] = useState(false);
+    const [list, setList] = useState(null);
+
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
-    const [list, setList] = useState(null);
     useEffect(() => {onLoad()}, []);
 
     async function onLoad() {
       await getUserByClass(props.id)
-      .then(data => (data.length == 1)?
-      setList(
-          <ListGroup.Item key="nothing">
-            <strong>No Available Classmate at this point :(</strong>
-          </ListGroup.Item>
+      .then
+      (
+        data => (data.length == 1)?
+        setList
+        (
+            <ListGroup.Item key="nothing">
+              <strong>No Available Classmate :(</strong>
+            </ListGroup.Item>
+        )
+      :
+        setList
+        (
+          data
+          .filter(user => user.id !== props.userId)  //filter the array without current user
+          .map((user) => 
+          <LinkContainer to={{pathname:"/message", aboutProps: user.id}}>
+            <ListGroup.Item key={user.id}>
+              {user.id}
+            </ListGroup.Item>
+          </LinkContainer>)
+        )
       )
-      :setList(
-        data
-        .filter(user => user.id !== props.userId)  //filter the array without current user
-        .map((user) => 
-        <LinkContainer to={{pathname:"/message", aboutProps: user.id}}>
-          <ListGroup.Item key={user.id}>
-            {user.id}
-          </ListGroup.Item>
-        </LinkContainer>)
-        ))
-      .catch(err => alert(err));
+        .catch(err => alert(err));
     }
  
-
     return (
         <>
           <Button block variant="outline-primary" onClick={handleShow}>
             {props.name}
           </Button>
-         
-    
+      
           <Modal show={show} onHide={handleClose}>
             <Modal.Header closeButton>
               <Modal.Title>{props.name} Class Info</Modal.Title>
